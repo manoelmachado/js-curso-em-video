562535a71df90831247fd8a7f411d4182a5bf691
manoelmachado <m.machado49@hotmail.com>
2022-08-01 17:55:54 -0300

    aulas e exercícios de JS

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 65
+console.log(`A velocidade do seu carro é ${vel}Km/h.`)
+if (vel > 60) { // condição simples
+    console.log('Você ultrapassou a velocidade permitida, multado!')
+}
+console.log('Diriga sempre com o cinto de segurança.')
