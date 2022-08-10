6ca34e232ab71e9048b3a25ecfa322ffc8012e45
manoelmachado <m.machado49@hotmail.com>
2022-08-10 16:01:24 -0300

    repetições parte 1

--- ex014.js
+++ ex014.js
@@ -9,7 +9,7 @@ console.log('Tudo bem?')
 console.log('Tudo bem?')
 
 /*
-Da maneira abaixo reproduzimos uma quantidade maior de repetições e com um códígo bem mais simples e limpo.
+Da maneira abaixo reproduzimos uma quantidade maior de repetições e com um códígo bem mais simples e limpo. Declaramos uma condição, o código testa, executa e faz o looping.
 sintaxe:
 
 while (condição){
@@ -24,5 +24,17 @@ while (c <= 6) {
 }
 
 /*
-Existe outra maneira de se realizar o mesmo comando com uma sintaxe diferente, para ser usado em casos específicos
+Existe outra maneira de se realizar a repetição com uma sintaxe diferente, para ser usado em casos específicos. O código executa, testa a condição e faz o looping.
+Sintaxe: 
+
+do {
+
+} while (condicao)
+
 */
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
