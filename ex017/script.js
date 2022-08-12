8dc4ef8d62a8fb2f2a18cb8549e27a8b0c356d22
manoelmachado <m.machado49@hotmail.com>
2022-08-12 17:26:54 -0300

    +1 exercício

---
+++ ex017/script.js
@@ -0,0 +1,22 @@
+function calcular () {
+    var num = document.getElementById('txtn')
+    var r = document.getElementById('res') 
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        r.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            r.appendChild(item)
+            c++
+        }
+
+
+    }
+    
+
+
+}
